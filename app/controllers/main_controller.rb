5e22b77e7f07d585ebe9a58eea927e3b280d0e11
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-05-23 00:34:03 +0300

    -

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   def index
   end
   def projects
-
+  end
+  def rules
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'main/index'
   get 'main/projects'
+  get 'main/rules'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
