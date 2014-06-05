1bfe8b5e101af8c28c1b195ca3fdb28b27461686
Alexey Ustyantsev <hudbrog@gmail.com>
2014-06-05 21:57:37 +0300

    First prod push

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :rails_env, 'production'
 
 server 'hackerspace.by:2220', :app, :web, :db, :primary => true
-set :deploy_to, '/srv/www/dev.hackerspace.by'
+set :deploy_to, '/srv/www/hackerspace.by'
 
 set :branch, 'master' unless exists?(:branch)
 set :user, 'hudbrog'  # The server's user for deploys
