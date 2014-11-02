b7629aecd2780d8b0feed2f80d6b1253f99caa13
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-11-02 15:29:43 +0300

    some css fixed, seed.rb file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Project.destroy_all
-
 unless Rails.env.production?
+  Project.destroy_all
+  User.destroy_all
+
+  User.create(email: 'developer@hackerspace.by', password: '111111')
+  User.create(email: 'developer2@hackerspace.by', password: '111111')
+
   7.times do
-    Project.create(name: Faker::Commerce.product_name, short_desc: Faker::Lorem.paragraph(rand(2..4)), full_desc: Faker::Lorem.paragraph(rand(7..20)))
+    Project.create!(name: Faker::Commerce.product_name,
+                    short_desc: Faker::Lorem.paragraph(rand(2..4)),
+                    full_desc: Faker::Lorem.paragraph(rand(7..20)),
+                    user: User.all.sample,
+                    photo: File.open(Dir['app/assets/images/*.jpg'].sample))
   end
 end
