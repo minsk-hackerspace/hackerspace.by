25c28fe8ffa4116759090f860de8bb54be7718d2
Alex Gutikov <gutikoff@gmail.com>
2014-11-29 00:38:32 +0300

    fix syntax error

--- app/models/project.rb
+++ app/models/project.rb
@@ -30,7 +30,7 @@ class Project < ActiveRecord::Base
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   validates_attachment :photo, presence: true, size: { in: 0..3.megabytes }
 
-  validates :name, :short_desc, :project_status presence: true
+  validates :name, :short_desc, :project_status, presence: true
   validates :markup_type, inclusion: SUPPORTED_MARKUPS
 
 end
