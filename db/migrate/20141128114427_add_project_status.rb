260a770ed3287f236f0e623022394492f06ece33
Alex Gutikov <gutikoff@gmail.com>
2014-11-29 00:36:22 +0300

    revieved: new sintax in migration

--- db/migrate/20141128114427_add_project_status.rb
+++ db/migrate/20141128114427_add_project_status.rb
@@ -1,5 +1,5 @@
 class AddProjectStatus < ActiveRecord::Migration
   def change
-    add_column :projects, :project_status, :string, :default => 'активный'
+    add_column :projects, :project_status, :string, default: 'активный'
   end
 end
