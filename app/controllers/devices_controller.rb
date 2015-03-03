ac32e846182fad2625a82b5217238116a3e74327
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-03-04 00:33:50 +0300

    devices list added, events folding feature, rails debug gem changed

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,14 @@
+class DevicesController < ApplicationController
+  def index
+    @devices = Device.all
+  end
+
+  def show
+    @device = Device.find(params[:id])
+    @events = @device.events_folded
+    respond_to do |format|
+      format.html { render :show }
+      format.csv { render csv: @events, filename: 'events' }
+    end
+  end
+end
