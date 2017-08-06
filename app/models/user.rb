429b8f32d81ea3ff45aaad253ca4cfc296aa8243
Yauhen Kharuzhy <jekhor@gmail.com>
2017-08-06 13:03:37 +0300

    Set ERIP payment amount corresponding to user's setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,7 +100,7 @@ class User < ApplicationRecord
 
     bp = BePaid::BePaid.new Setting['bePaid_baseURL'], Setting['bePaid_ID'], Setting['bePaid_secret']
 
-    amount = 50.00
+    amount = user.monthly_payment_amount
 
     #amount is (amoint in BYN)*100
     bill = {
