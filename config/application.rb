4a070603c8008ff4f1c986b800b4cc1df538d2ab
Yauhen Kharuzhy <jekhor@gmail.com>
2018-03-05 18:37:54 +0300

    Set timezone to Minsk

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Hspace
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Minsk'
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
