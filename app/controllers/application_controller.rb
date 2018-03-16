00a503b61a94ed828f87e6f891822ce374a250b8
Mikhail Chuprysnki <k2m30@users.noreply.github.com>
2018-03-16 11:21:37 +0300

    Revert "Kitty number balance health"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   before_action :check_if_hs_open if Rails.env.production?
   before_action :check_for_present_people if Rails.env.production?
   before_action :check_for_hs_balance
-  before_action :calc_kitty_number
   before_action :get_transactions
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -34,26 +33,10 @@ class ApplicationController < ActionController::Base
     @hs_present_people.uniq!
   end
 
-    def calc_kitty_number
-    if user_signed_in?
-        @hs_kitty_number = Rails.cache.fetch "hs_kitty_number", expires_in: 3.hours do
-            three_months_ago_date = Time.now - 3.months.ago
-            transactions = BankTransaction.where('created_at > ?', three_months_ago_date)
-            the_sum = transactions.sum(:minus)
-            (100 * @hs_balance / the_sum).round(1)
-
-        end
-    end
-    end
-
   def check_for_hs_balance
     if user_signed_in?
       @hs_balance = Rails.cache.fetch "hs_balance", expires_in: 3.hours do
-        if Rails.env.development? or Rails.env.test? then
-            7777
-        else
-            Belinvestbank.fetch_balance unless Rails.env.development? or Rails.env.test?
-        end
+        Belinvestbank.fetch_balance unless Rails.env.development? or Rails.env.test?
       end
     end
   end
