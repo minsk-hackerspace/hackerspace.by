323ae6b2f4caf9a3e3aa7019165e57ec29550381
AliakseiLeanovich <aliaksei.leanovich@gmail.com>
2018-09-01 01:50:22 +0300

    Update spec

--- spec/services/notifications_service_spec.rb
+++ spec/services/notifications_service_spec.rb
@@ -6,15 +6,19 @@ describe NotificationsService do
     let!(:user_with_outdated_payment) { create :user, :with_payment }
     let!(:user_with_valid_payment) { create :user, :with_valid_payment }
 
+    let(:debitors) { described_class.new.debitors }
+    let(:notified_emails) { ActionMailer::Base.deliveries.map(&:to).flatten }
+
     it 'notifies only debitors' do
-      expect(described_class.new.debitors).to eq([user_with_outdated_payment])
+      expect(debitors).to include(user_with_outdated_payment)
+      expect(debitors).not_to include(user_without_payment)
+      expect(debitors).not_to include(user_with_valid_payment)
     end
 
-    it 'sends notification email to each debitor' do
+    it 'sends notification email to debitors' do
       described_class.notify_debitors
 
-      expect(ActionMailer::Base.deliveries.count).to eq(1)
-      expect(ActionMailer::Base.deliveries.first.to).to eq([user_with_outdated_payment.email])
+      expect(notified_emails).to include(user_with_outdated_payment.email)
     end
   end
 end
