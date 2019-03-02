78b42134d7650b4462b1bbe682c1468273962b58
Yauhen Kharuzhy <jekhor@gmail.com>
2019-03-02 18:34:05 +0300

    bepaid notifications: Don't send notification about unsuccessfull transactions
    If notification about unsuccessfull transaction has been received, don't
    send "Thanks for payment" mail to user.

--- app/controllers/admin/erip_transactions_controller.rb
+++ app/controllers/admin/erip_transactions_controller.rb
@@ -136,7 +136,9 @@ class Admin::EripTransactionsController < AdminController
     respond_to do |format|
       if @erip_transaction.save
         format.json { render :show, status: :created, location: admin_erip_transaction_url(@erip_transaction) }
-        NotificationsMailer.with(transaction: @erip_transaction).notify_about_payment.deliver_later unless @erip_transaction.user.nil?
+        unless @erip_transaction.user.nil? or @erip_transaction.status != "successful"
+          NotificationsMailer.with(transaction: @erip_transaction).notify_about_payment.deliver_later 
+        end
       else
         format.json { render json: @erip_transaction.errors, status: :unprocessable_entity }
       end
