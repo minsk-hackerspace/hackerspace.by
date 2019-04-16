361dbfafc7dc92c2ec0ac872ea20b30693136c73
Yahor Zhuchkou <jagor.z@gmail.com>
2019-04-16 21:19:53 +0300

    Reset password for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable,
-         #:recoverable, :rememberable,
+         :recoverable,
+         # :rememberable,
          :trackable
           # :validatable
   comma do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => "hackerspace.by" }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
