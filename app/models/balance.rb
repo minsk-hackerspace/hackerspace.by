d3f7a12e4cbc23a882fa15bb2c1909aadcfe87c3
Yauhen Kharuzhy <jekhor@gmail.com>
2019-09-19 23:26:04 +0300

    Graph: replace handmade selection of day balances by DB queries

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -16,10 +16,10 @@
 class Balance < ApplicationRecord
   def self.graph(start_date, end_date)
     Rails.cache.fetch [start_date, end_date, :balance_graph] do
-      balances = Balance.where(created_at: [start_date..end_date]).to_a
       graph = []
-      (start_date..end_date).to_a.each do |date|
-        state = balances.select {|b| b.created_at >= date.beginning_of_day and b.created_at <= date.end_of_day}.last.try(:state)
+      (start_date..end_date).each do |date|
+        state = Balance.where('created_at >= ? AND created_at <= ?',
+                              date.beginning_of_day, date.end_of_day).last.try(:state)
         graph << [date.strftime('%d-%b-%y'), state || graph.last.try(:last) || 0]
       end
       graph
