81b475da77913c6725d93169ee3fcfda5edc6373
Yauhen Kharuzhy <jekhor@gmail.com>
2019-10-03 23:41:01 +0300

    Make cache expired every bank balance and transaction fetching process

--- app/services/bank_fetcher_service.rb
+++ app/services/bank_fetcher_service.rb
@@ -1,6 +1,6 @@
 class BankFetcherService
   def self.fetch_balance
-    Rails.cache.fetch "hs_balance", expires_in: 3.hours do
+    Rails.cache.fetch "hs_balance", expires_in: 0.5.hours do
       begin
         Belinvestbank.fetch_balance unless Rails.env.development? or Rails.env.test?
       rescue => e
@@ -12,7 +12,7 @@ class BankFetcherService
   end
 
   def self.fetch_transactions
-    Rails.cache.fetch "bank_transactions", expires_in: 24.hours do
+    Rails.cache.fetch "bank_transactions", expires_in: 2.hours do
       begin
         BankTransaction.get_transactions
         Balance.where(state: 0).ids.each { |i| Balance.find(i).update(state: Balance.find(i-1).state) }
