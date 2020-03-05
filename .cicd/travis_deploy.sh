2747e425b54e3a32f63fdaa1ac4b7e1f8d8ec8b2
Anatoli Babenia <anatoli@rainforce.org>
2020-03-05 19:13:42 +0300

    Fix deploy again. Fix deploy_key path, fail on err
    https://github.com/minsk-hackerspace/hackerspace.by/pull/430#issuecomment-595307561

--- .cicd/travis_deploy.sh
+++ .cicd/travis_deploy.sh
@@ -3,16 +3,20 @@
 # enable tracing
 set -x
 
+# fail on first error
+set -e
+
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+KEYPATH="$THISDIR/deploy_key"
 
 echo [x] Setup SSH...
 echo -e "Host hackerspace.by\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo Decrypting SSH key for Travis...
-openssl aes-256-cbc -K $encrypted_622d28529b58_key -iv $encrypted_622d28529b58_iv -in "$THISDIR/deploy_key.enc" -out "$THISDIR/deploy_key" -d
-chmod 600 ./deploy_key
+openssl aes-256-cbc -K $encrypted_622d28529b58_key -iv $encrypted_622d28529b58_iv -in "$THISDIR/deploy_key.enc" -out "$KEYPATH" -d
+chmod 600 "$KEYPATH"
 eval "$(ssh-agent -s)"
-ssh-add ./deploy_key
-ssh -i ./deploy_key mhs@hackerspace.by pwd
+ssh-add "$KEYPATH"
+ssh -i "$KEYPATH" mhs@hackerspace.by pwd
 
 
 echo "[x] Preparing and running 'mina' (Ruby deploy tool)"
