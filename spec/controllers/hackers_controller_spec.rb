259db59039b867ffa0b01ddfc175a091624a9ffa
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2020-03-07 23:26:32 +0300

    ruby version fixed

--- spec/controllers/hackers_controller_spec.rb
+++ spec/controllers/hackers_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe HackersController, type: :controller do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryBot.create(:user) }
 
   before do
     sign_in user
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
