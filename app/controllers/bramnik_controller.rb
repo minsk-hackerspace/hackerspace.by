bd96b87a95b3804156d66bf5b4f0cec69318c2b8
Yauhen Kharuzhy <jekhor@gmail.com>
2021-09-20 21:11:57 +0300

    bramnik endpoint: export all users to ensure that bramnik deny access for blocked hackers

--- app/controllers/bramnik_controller.rb
+++ app/controllers/bramnik_controller.rb
@@ -2,7 +2,7 @@ class BramnikController < ActionController::API
 
   def index
     if authenticate_token
-      @users = User.active.map { |u| {id: u.id, name: "#{u.first_name} #{u.last_name}", paid_until: u.paid_until, access_allowed: u.access_allowed?, nfc_keys: u.nfc_keys.pluck(:body)} }
+      @users = User.all.map { |u| {id: u.id, name: "#{u.first_name} #{u.last_name}", paid_until: u.paid_until, access_allowed: u.access_allowed?, nfc_keys: u.nfc_keys.pluck(:body)} }
       render json: @users, status: :ok
     else
       render plain: "Authorization required\n", status: :unauthorized
