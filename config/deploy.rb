8d3f6930dbda678cc89842469d422e9ae244507d
Yauhen Kharuzhy <jekhor@gmail.com>
2022-05-17 22:00:42 +0300

    Use ruby 3.1.2 for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,7 +70,7 @@ end
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :remote_environment do
-  invoke :'rvm:use', 'ruby-3.0'
+  invoke :'rvm:use', 'ruby-3.1.2'
 end
 
 desc 'Deploys the current version to the server.'
