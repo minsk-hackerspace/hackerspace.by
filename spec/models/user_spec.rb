44285a1e7a4a3b566c400653a47caa8e78d0718d
Yahor Zhuchkou <jagor.z@gmail.com>
2023-12-22 09:25:50 +0200

    Set full tariff for user by default

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,6 +60,10 @@ describe User, type: :model  do
 
     it { should validate_presence_of(:email) }
     it { should validate_length_of(:email).is_at_most(255) }
+
+    it 'has default tariff' do
+      expect(user.tariff).not_to be_blank
+    end
   end
 
   describe '.telegram_username' do
