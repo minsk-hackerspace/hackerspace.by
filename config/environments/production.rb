1c0ad516e746cf93c9fecfef8ae8dd7e11bb164e
Yauhen Kharuzhy <jekhor@gmail.com>
2024-05-16 22:22:04 +0300

    SMTP server: change port to 465

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     domain: 'hackerspace.by',
     authentication: 'plain',
     enable_starttls_auto: true,
