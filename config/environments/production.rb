16f4acb70dfef3f3da93864a8dd73822b26d22d2
Yauhen Kharuzhy <jekhor@gmail.com>
2024-05-16 23:02:41 +0300

    SMTP server: switch to gmail smtp relay

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp-relay.gmail.com',
     port: 465,
     domain: 'hackerspace.by',
     authentication: 'plain',
