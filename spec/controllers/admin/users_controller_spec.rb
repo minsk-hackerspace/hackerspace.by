697d4b8b5c311a1d1174f6c91445e55f01ca68b9
Yahor Zhuchkou <jagor.z@gmail.com>
2025-04-30 19:05:59 +0300

    admin/users_controller.rb specs and cleanup

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,16 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
-  let(:user) { FactoryBot.create(:admin_user) }
+  let(:admin) { FactoryBot.create(:admin_user) }
+  let(:tariff) { create :tariff }
 
-  before do
-    sign_in user
-  end
+  context 'Logged in admin user' do
+    before do
+      sign_in admin
+    end
 
-  describe "GET #new" do
-    it "returns http success" do
-      process :new
-      expect(response).to have_http_status(:success)
+    describe "GET #new" do
+      it "returns http success" do
+        process :new
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET 'show'" do
+      xit "returns http success" do
+        process :show, params: { id: admin.id }
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET 'edit'" do
+      xit "returns http success" do
+        process :edit, params: { id: admin.id }
+        expect(response).to be_successful
+      end
+    end
+
+    describe "POST 'create'" do
+      it "returns http success create and redirect" do
+        post :create, params: {
+          user: { email: "example#{Time.now.to_i}@gmail.com", first_name: 'name', last_name: 'last', tariff_id: tariff.id }
+        }
+        expect(response).to redirect_to(users_path)
+      end
+
+      it "returns new page if not created" do
+        post :create, params: { user: { name: 'test' } }
+        expect(response).to render_template("new")
+      end
     end
   end
 end
