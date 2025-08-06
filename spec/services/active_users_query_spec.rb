d40e5938e6cf580e9dfbadfe6ae2d2557ab2e72f
Yahor Zhuchkou <jagor.z@gmail.com>
2025-08-06 10:21:59 +0300

    active_users_query_spec.rb fix for test data

--- spec/services/active_users_query_spec.rb
+++ spec/services/active_users_query_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe ActiveUsersQuery do
   describe '.perform' do
+    let!(:user_1) { create(:user, :with_payment) }
+    let!(:user_2) { create(:user, :with_payment) }
+
     it 'returns deafult not epmpty users set' do
       expect(described_class.new(order: nil).perform).not_to be_empty
     end
